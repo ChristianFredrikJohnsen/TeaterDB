1b57a8e72638fb6696f729839e0409dd35f1d2c9
Christian Fredrik Johnsen <christian@johnsen.no>
2024-03-05 16:15:09 +0100

    feat: Add tables Teatersal, Område, and Stol to the database and insert data into Kundeprofil, Teatersal, Område, and Stol tables

--- create_database.py
+++ create_database.py
@@ -1,5 +1,6 @@
 import sqlite3
 from icecream import ic
+
 connection = sqlite3.connect('teater.db')
 c = connection.cursor()
 
@@ -8,18 +9,34 @@ CREATE TABLE if NOT EXISTS Kundeprofil
 (id INTEGER PRIMARY KEY, Navn STRING, Adresse STRING, Telefonnummer STRING);
 """)
 
+# Add foreign key to Fremvisning at a later stage.
 c.execute("""
-INSERT INTO Kundeprofil (Navn, Adresse, Telefonnummer)
-VALUES ('Kari Nordmann', 'Osloveien8', '90015308');
+CREATE TABLE IF NOT EXISTS Teatersal
+    (SalNummer INTEGER PRIMARY KEY, AntallPlasser INTEGER);
 """)
 
-ic(c.execute("""SELECT * FROM Kundeprofil;""").fetchall())
+
+c.execute("""
+    CREATE TABLE IF NOT EXISTS Område
+        (id INTEGER PRIMARY KEY, OmrådeNavn TEXT);
+""")
+
+c.execute("""
+CREATE TABLE IF NOT EXISTS Stol
+    (RadNummer INTEGER, StolNummer INTEGER, SalNummer INTEGER, OmrådeID INTEGER,
+    FOREIGN KEY (SalNummer) REFERENCES Teatersal(SalNummer)
+    FOREIGN KEY (OmrådeID) REFERENCES Område(id),
+    PRIMARY KEY (RadNummer, StolNummer, SalNummer, OmrådeID));
+""")
 
 
 
 def cleanup():
     c.execute("DROP TABLE Kundeprofil;")
+    c.execute("DROP TABLE Teatersal;")
+    c.execute("DROP TABLE Område;")
+    c.execute("DROP TABLE Stol;")
     connection.close()
 
-
-cleanup()
+connection.commit()
+# cleanup()
