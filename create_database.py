28fc11c164cbe8d086a8a275c875ff2d083fefc0
Kacper Pawlowski <kacperp@stud.ntnu.no>
2024-03-05 16:17:34 +0100

    "Added tables for Akt, Ansettelse, Person, Roller, Ansvar og Teaterstykke"

--- create_database.py
+++ create_database.py
@@ -13,8 +13,77 @@ INSERT INTO Kundeprofil (Navn, Adresse, Telefonnummer)
 VALUES ('Kari Nordmann', 'Osloveien8', '90015308');
 """)
 
+
+c.execute("""CREATE TABLE Person (
+    PersonID        INTEGER PRIMARY KEY AUTOINCREMENT
+                            UNIQUE
+                            NOT NULL,
+    Email           TEXT    NOT NULL
+                            UNIQUE,
+    Name            TEXT    NOT NULL,
+    AnsettelsesForm INTEGER REFERENCES Ansettelse (AnsettelseID) 
+                            NOT NULL
+);""")
+
+
+c.execute("""CREATE TABLE Ansettelse (
+    AnsettelseID INTEGER UNIQUE
+                         NOT NULL
+                         PRIMARY KEY AUTOINCREMENT,
+    Type                 UNIQUE
+                         NOT NULL
+);""")
+
+
+c.execute("""CREATE TABLE Akt (
+    Aktnummer      INTEGER PRIMARY KEY
+                           UNIQUE
+                           NOT NULL,
+    Navn           TEXT,
+    TeaterstykkeID INTEGER REFERENCES Teaterstykke (TeaterstykkeID) 
+                           NOT NULL
+);""")
+
+
+c.execute("""CREATE TABLE Teaterstykke (
+    TeaterstykkeID     INTEGER PRIMARY KEY AUTOINCREMENT
+                   UNIQUE
+                   NOT NULL,
+    Navn   TEXT    NOT NULL,
+    Sesong TEXT    NOT NULL
+);""")
+
+c.execute("""CREATE TABLE Ansvar (
+    PersonID       INTEGER REFERENCES Person (PersonID) 
+                           NOT NULL,
+    TeaterstykkeID INTEGER REFERENCES Teaterstykke (TeaterstykkeID) 
+                           NOT NULL,
+    Ansvarsområde  TEXT    NOT NULL,
+    PRIMARY KEY (
+        PersonID,
+        TeaterstykkeID,
+        Ansvarsområde
+    )
+);""")
+
+c.execute("""CREATE TABLE Roller (
+    PersonID  INTEGER REFERENCES Person (PersonID) 
+                      NOT NULL,
+    AktNummer INTEGER NOT NULL
+                      REFERENCES Akt (Aktnummer),
+    Rollenavn         UNIQUE
+                      NOT NULL,
+    PRIMARY KEY (
+        PersonID,
+        AktNummer,
+        Rollenavn
+    )
+);""")
+
+
 ic(c.execute("""SELECT * FROM Kundeprofil;""").fetchall())
 
+connection.commit()
 
 
 def cleanup():
