53b9fbc2bc1f3b0df99eea5383c363f166cbac4f
oskarew <oskarew@stud.ntnu.no>
2024-03-05 16:16:55 +0100

    [Feat] Kundeprofil og Billettkjøp tabeller.

--- create_database.py
+++ create_database.py
@@ -1,25 +1,50 @@
 import sqlite3
 from icecream import ic
-connection = sqlite3.connect('teater.db')
+
+connection = sqlite3.connect("teater.db")
 c = connection.cursor()
 
-c.execute("""
-CREATE TABLE if NOT EXISTS Kundeprofil
-(id INTEGER PRIMARY KEY, Navn STRING, Adresse STRING, Telefonnummer STRING);
-""")
 
-c.execute("""
-INSERT INTO Kundeprofil (Navn, Adresse, Telefonnummer)
-VALUES ('Kari Nordmann', 'Osloveien8', '90015308');
-""")
+### Bilett avdelingen kommer her ###
+c.execute(
+    """
+    CREATE TABLE if NOT EXISTS Kundeprofil (
+    id INTEGER PRIMARY KEY, 
+    Navn STRING, 
+    Adresse STRING, 
+    Telefonnummer INTEGER);
+    """
+)
+c.execute(
+    """
+    CREATE TABLE if NOT EXISTS Billettkjop 
+        (id INTEGER PRIMARY KEY, 
+        KundeID INTEGER NOT NULL, 
+        Kjopstidspunkt TIMESTAMP, 
+        FOREIGN KEY (KundeID) REFERENCES Kundeprofil(id))
+        ON DELETE CASCADE ON UPDATE CASCADE;
+    """
+)
+
+c.execute(
+    """
+    CREATE TABLE if NOT EXISTS Billett 
+        (
+    """
+)
+
+#### Insert some rows of data ####
+c.execute(
+    """INSERT INTO Kundeprofil (Navn, Adresse, Telefonnummer) VALUES ('Ola Nordmann', 'Nordveien 1', 12345678);"""
+)
 
 ic(c.execute("""SELECT * FROM Kundeprofil;""").fetchall())
 
 
-
 def cleanup():
     c.execute("DROP TABLE Kundeprofil;")
+    c.execute("DROP TABLE Billettkjop;")
     connection.close()
 
 
-cleanup()
+# cleanup()
